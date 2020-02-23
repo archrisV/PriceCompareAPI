2d92844e5496f58bba5d222f24f91328a6797b75
Christian Torbica <uplink.chris@gmail.com>
2020-02-23 20:20:08 +0100

    Added DbContext for classes

---
+++ PriceCompareAPI/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PriceCompareAPI.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ PriceCompareAPI/Models/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PriceCompareAPI.Models
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Store> Stores { get; set; }
+
+    }
+}
