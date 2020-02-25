a87016867a78324dd3682c444d2b9e5d8129db74
Christian Torbica <uplink.chris@gmail.com>
2020-02-25 13:57:55 +0100

    Added DataContext

---
+++ PriceCompareAPI/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCompareAPI.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+    }
+}
